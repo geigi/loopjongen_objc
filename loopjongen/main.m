d3e86dc899b39012bdb6998e0aafe596f05eb44e
Geigi <git@geigi.de>
2016-02-07 10:51:03 +1100

    Initial Commit

---
+++ loopjongen/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  loopjongen
+//
+//  Created by Julian Geywitz on 07/02/16.
+//  Copyright © 2016 Julian Geywitz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
